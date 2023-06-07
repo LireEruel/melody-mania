4f6e2653422e87e415aab2860f545c02cbedb1f8
LireEruel <jsh001505@gmail.com>
2023-06-07 13:51:57 +0900

    main 에서 cloudinary 삭제

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import Cloudinary from 'cloudinary'
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
@@ -14,12 +13,5 @@ const app = createApp(App)
 app.use(pinia)
 app.use(Antd)
 app.use(router)
-app.use(Cloudinary, {
-  configuration: {
-    cloud_name: 'dwn77lk2b',
-    api_key: '397288484875562',
-    api_secret: 'tqSgDpxqaHQQrPNu6M_OyA6N98Y'
-  }
-})
 
 app.mount('#app')
